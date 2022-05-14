f5cdd6294aa7310dc6dc886de4049462da158db6
osnim <alsth4@naver.com>
2022-05-14 22:58:05 +0900

    Create 네트워크.py

---
+++ DFS, BFS/Programmers/네트워크.py
@@ -0,0 +1,25 @@
+def solution(n, computers):
+    answer = n
+    visited = [0] * n
+    Linked = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if computers[i][j] == 0:
+                continue
+            Linked[i].append(j)
+
+    for i in range(n):
+        q = Linked[i][:]
+        while q:
+            adj = q.pop(0)
+            if adj <= i:
+                continue
+            if visited[adj]:
+                continue
+            answer -= 1
+            q = q + Linked[adj]
+            visited[adj] = 1
+
+    return answer
