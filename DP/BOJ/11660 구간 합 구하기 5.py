26b540c8c8bb5e0ce868a081dc409c4e08f9e750
osnim <alsth4@naver.com>
2022-05-22 23:58:01 +0900

    Update 11660 구간 합 구하기 5.py

--- DP/BOJ/11660 구간 합 구하기 5.py
+++ DP/BOJ/11660 구간 합 구하기 5.py
@@ -4,10 +4,7 @@ arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 dp = [[0]*(n+1) for i in range(n+1)]
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if i == 1 or j == 1:
-            dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1]
-        else:
-            dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1]
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1]
 
 for i in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
