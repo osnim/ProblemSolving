600104ecd46c514db63ea353b27544369832b085
osnim <alsth4@naver.com>
2022-05-22 23:57:27 +0900

    Create 11659 구간 합 구하기 4.py

---
+++ DP/BOJ/11659 구간 합 구하기 4.py
@@ -0,0 +1,9 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+dp = [0]*(n+1)
+for i in range(1, n+1):
+    dp[i] = dp[i-1] + arr[i-1]
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    print(dp[j] - dp[i-1])
