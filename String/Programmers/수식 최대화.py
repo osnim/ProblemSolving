655098582b2828ae50d144c5a0acd4a6dff7b724
osnim <alsth4@naver.com>
2022-06-09 12:10:57 +0900

    Update 수식 최대화.py

--- String/Programmers/수식 최대화.py
+++ String/Programmers/수식 최대화.py
@@ -1,19 +1,17 @@
 import re
 from itertools import permutations
-def compute(expression, p, i):
-    return
-
 def solution(expression):
     answer = 0
     per = list(permutations(["+", "-", "*"], 3))
-    ex = re.split("([\+\-\*])", expression)
-    print(ex)
+    expression = re.split("([\+\-\*])", expression)
+    print(expression)
     for p in per:
+        ex = expression[:]
         for op in p:
-            if op in ex:
-                i = ex.index(op) # op의 인덱스를 찾음
-                ex[i-1] = str(eval(ex[i-1] + ex[i] + ex[i+1]))
-                del ex[i:i+2]
-                print(ex)
+            while op in ex:
+                i = ex.index(op)  # op의 인덱스를 찾음
+                ex[i - 1] = str(eval(ex[i - 1] + ex[i] + ex[i + 1]))
+                del ex[i:i + 2]
 
+        answer = max(answer, abs(int("".join(ex))))
     return answer
