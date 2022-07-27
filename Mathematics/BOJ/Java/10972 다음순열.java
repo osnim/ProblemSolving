f057801517bf4c793fab4e0b3ff7b246a72a574f
osnim <alsth4@naver.com>
2022-07-27 22:49:17 +0900

    폴더 이동

---
+++ Mathematics/BOJ/Java/10972 다음순열.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main{
+	
+	static int[] p;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		
+		StringTokenizer stz = new StringTokenizer(sc.nextLine());
+		
+		p = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			p[i] = Integer.parseInt(stz.nextToken());
+		}
+		if(np(n-1)) {
+			for (int i = 0; i < n; i++) {
+				System.out.print(p[i]+" ");
+			}
+		}else {
+			System.out.println(-1);
+		}
+
+	}
+	private static boolean np(int size) {
+		// 맨 오른쪽 이동 피크 찾기
+		int i = size; 
+		while(i>0 && p[i-1] > p[i]) i--;
+		if(i == 0) return false; //이미정렬
+		
+		// 피크 왼쪽보다 가장 큰 수를 오른쪽부터 찾아서 스왑 
+		int j = size;
+		while(p[i-1] > p[j]) j--;
+		int temp = p[i-1];
+		p[i-1] = p[j];
+		p[j] = temp;
+		
+		//무조건 스왑
+		int k= size;
+		while(i < k) {
+			temp = p[i];
+			p[i] = p[k];
+			p[k] = temp;
+			i++;
+			k--;
+		}
+		return true;
+	}
+}
