4bf34d42fd3a39d912e00cb606389490082a103b
osnim <alsth4@naver.com>
2022-07-29 00:07:33 +0900

    Update 2294 동전 2.py

--- Dynamic Programming/BOJ/2294 동전 2.py
+++ Dynamic Programming/BOJ/2294 동전 2.py
@@ -3,9 +3,15 @@ arr = set()
 dp = [10001]*(k+1)
 dp[0] = 0
 for _ in range(n):
-    arr.add(int(input))
+    arr.add(int(input()))
 
-for i in range(1, k+1):
-    for j in arr:
-        dp[i - j] = ,
+arr = list(arr)
+for i in range(len(arr)):
+    for j in range(arr[i], k+1):
+        if dp[j - arr[i]] != 10001:
+            dp[j] = min(dp[j], dp[j - arr[i]]+1)
 
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
