4c57a827c11514c4a300c68a7ccad9d0fced89df
osnim <alsth4@naver.com>
2022-08-04 22:57:54 +0900

    폴더 생성 및 수정

---
+++ Stack, Queue/BOJ/Java/2164 카드2.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_2164_카드2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Queue<Integer> q = new LinkedList<>();
+		
+		for (int i = 1; i <= n; i++) {
+			q.offer(i);
+		}
+		
+		while(q.size() > 1) {
+			q.poll();
+			q.offer(q.poll());
+		}
+		System.out.println(q.peek());
+	}
+
+}
