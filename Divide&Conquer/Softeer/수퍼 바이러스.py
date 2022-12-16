6c0bcd9ffcb14d228cc349e5600f188c62d5b237
김민소 <alsth4@naver.com>
2022-12-17 00:06:00 +0900

    Update 수퍼 바이러스.py

--- Divide&Conquer/Softeer/수퍼 바이러스.py
+++ Divide&Conquer/Softeer/수퍼 바이러스.py
@@ -1,23 +1,20 @@
 import sys
 input = sys.stdin.readline
-#print = sys.stdout.write
 MOD = 1000000007
 
 K, P, N = map(int, input().split()) # 처음 바이러스 수, 증가율, 총 시간
 
-K %= MOD
-P %= MOD
-N %= (N*10)
+N = (N*10)
 
 def DivideAndConquer(p, n):
-    if p == 0:
+    if n == 0:
         return 1
 
-    if p%2 == 1:
-        return p * (DivideAndConquer(p-1, n) % MOD)
+    if n%2 == 1:
+        return p * (DivideAndConquer(p, n-1)) % MOD
 
     else:
-        half = (DivideAndConquer(p//2, n) % MOD)
+        half = (DivideAndConquer(p, n//2)) % MOD
         return half * half % MOD
 
-print((k * DivideAndConquer(P, N)) % MOD)
+print((K * DivideAndConquer(P, N)) % MOD)
