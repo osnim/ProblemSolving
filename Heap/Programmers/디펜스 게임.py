039145c260e50258c4268b42074762cba44c58fb
김민소 <alsth4@naver.com>
2023-01-02 14:00:33 +0900

    Create 디펜스 게임.py

---
+++ Heap/Programmers/디펜스 게임.py
@@ -0,0 +1,30 @@
+import heapq as hq
+
+def solution(n, k, enemy):
+    global answer
+    answer = k  # 최소 k개 이상 가능
+    q = []
+    end = len(enemy)
+    left = 0
+    while left < end:
+        e = enemy[left]
+        # print(n, q, left+1)
+        if n >= e:  # 병사가 적보다 많은 경우
+            hq.heappush(q, -e)
+            n -= e
+
+        else:  # 병사가 적보다 적은 경우
+            if k > 0:  # 무적권을 사용할 수 있는 경우
+                k -= 1
+                if q:
+                    maxEnemy = -hq.heappop(q)
+                    if e > maxEnemy:
+                        hq.heappush(q, -maxEnemy)
+                    else:
+                        n += maxEnemy
+                        n -= e
+                        hq.heappush(q, -e)
+            else:
+                break  # 무적권도 없고 병사도 적은 경우
+        left += 1
+    return left
